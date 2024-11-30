e9a9fce260e372c5c1fee95d47f78667d34ba913
dirk <dirkchaney4@gmail.com>
2024-11-30 00:11:08 -0500

    Initial Commit

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ const path = require('node:path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 300,
+    height: 400,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
-    }
+    },
+    autoHideMenuBar: true 
   })
 
   // and load the index.html of the app.
--- renderer.js
+++ renderer.js
@@ -5,3 +5,25 @@
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
  */
+
+let timer = 0;
+let timerInterval;
+const button = document.getElementById('start-button');
+
+document.getElementById('start-button').addEventListener('click', () => {
+    // Check if the timer is already running
+    if (timerInterval) {
+        clearInterval(timerInterval);
+        timerInterval = null;
+        button.textContext = "Start"
+    } 
+    else{
+  
+    // Start the timer
+        timerInterval = setInterval(() => {
+        timer++;
+        document.getElementById('timer').textContent = timer;
+        }, 1000);
+        button.textContext = "Stop";
+    }
+  });
